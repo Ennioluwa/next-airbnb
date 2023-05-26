1f8c5bc78db7e420fedb1456204770f3785db028
ennioluwa <enioluwaarowo@gmail.com>
2023-05-26 07:07:14 +0100

    add listings feature

---
+++ app/components/inputs/Calendar.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { DateRange, Range, RangeKeyDict } from "react-date-range";
+
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+interface DatePickerProps {
+  value: Range;
+  onChange: (value: RangeKeyDict) => void;
+  disabledDates?: Date[];
+}
+
+const DatePicker: React.FC<DatePickerProps> = ({
+  value,
+  onChange,
+  disabledDates,
+}) => {
+  console.log(new Date());
+  const [defaultDate, setDefaultDate] = useState(new Date());
+  const disabledDatess = [
+    "2023-05-20",
+    "2023-05-21",
+    "2023-05-23",
+    "2023-05-24",
+    "2023-05-26",
+  ]; // Example disabled dates array
+
+  // useEffect(() => {
+  //   const firstNonDisabledDate = findFirstNonDisabledDate();
+  //   setDefaultDate(firstNonDisabledDate);
+  // }, []);
+
+  // const findFirstNonDisabledDate = () => {
+  //   let currentDate = new Date().toISOString().split("T")[0]; // Get current date in the format 'YYYY-MM-DD'
+  //   if (!disabledDates?.length) {
+  //     return new Date(currentDate);
+  //   }
+  const currentDate = new Date().toISOString().split("T")[0]; // Get current date in the format 'YYYY-MM-DD'
+  let nextDate = new Date(currentDate);
+  useEffect(() => {
+    let i;
+    for (i = 0; i < 20; i++) {
+      if (isDateDisabled(nextDate)) {
+        console.log("disabled");
+        nextDate.setDate(nextDate.getDate() + 1);
+        console.log(nextDate);
+      } else {
+        console.log("not disabled", nextDate);
+        return;
+      }
+    }
+    // while (isDateDisabled(nextDate)) {
+    //   nextDate.setDate(nextDate.getDate() + 1); // Add a day to the current date
+    // }
+    console.log(nextDate);
+  }, []);
+
+  const isDateDisabled = (date: Date) => {
+    const dateString = date.toISOString().split("T")[0]; // Convert date to 'YYYY-MM-DD' format
+    let dateDisabled = disabledDates?.map(
+      (data) => data.toISOString().split("T")[0]
+    );
+    console.log(dateDisabled, "disabled", dateString, "string");
+    console.log();
+
+    return dateDisabled?.includes(dateString);
+  };
+
+  // for (let date of disabledDates) {
+  //   if (date.toISOString() > currentDate) {
+  //     return new Date(date);
+  //   }
+  // }
+  // return null; // Return null if no non-disabled dates found
+  // };
+  // console.log(defaultDate ? new Date(defaultDate) : null);
+  // console.log(disabledDates?.length && disabledDates[0].toISOString());
+  // console.log(findFirstNonDisabledDate());
+
+  return (
+    <DateRange
+      rangeColors={["#262626"]}
+      ranges={[value]}
+      date={nextDate}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={nextDate}
+      disabledDates={disabledDates}
+    />
+  );
+};
+
+export default DatePicker;
--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className=" pb-20 pt-28"></div>
+        <div className=" pb-20 pt-20"></div>
         {children}
       </body>
     </html>
