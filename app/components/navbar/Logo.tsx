47ab4a7cbdd5ee2db900448889aa54532c7c9866
ennioluwa <enioluwaarowo@gmail.com>
2023-07-24 11:34:28 +0100

    make changes to the logo on mobile view

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,15 +6,26 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
-    <Image
-      priority
-      onClick={() => router.push("/")}
-      alt="Logo"
-      className="hidden md:block cursor-pointer"
-      height="100"
-      width="100"
-      src="/images/logo.png"
-    />
+    <div className="contents">
+      <Image
+        priority
+        onClick={() => router.push("/")}
+        alt="Logo"
+        className="hidden md:block cursor-pointer"
+        height="100"
+        width="100"
+        src="/images/logo.png"
+      />
+      <Image
+        priority
+        onClick={() => router.push("/")}
+        alt="Logo"
+        className="md:hidden block cursor-pointer"
+        height={30}
+        width={30}
+        src="/images/airbnb.png"
+      />
+    </div>
   );
 };
 
